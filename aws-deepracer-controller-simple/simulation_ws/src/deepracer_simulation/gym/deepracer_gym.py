Removing tf and adding a function to fix the yaw angle generation from quaternion

aws-deepracer-controller-simple/simulation_ws/src/deepracer_simulation/gym/deepracer_gym.py
@@ -6,7 +6,7 @@ from std_msgs.msg import Float32
 from std_msgs.msg import Float64
 from ackermann_msgs.msg import AckermannDriveStamped
 from gazebo_msgs.msg import ModelStates
-import tf
+#import tf
 from geometry_msgs.msg import PoseStamped
 from sensor_msgs.msg import LaserScan
 #from cvxpy import *
@@ -150,6 +150,30 @@ def get_clicked_point(data):
     """
 
 
+
+#https://automaticaddison.com/how-to-convert-a-quaternion-into-euler-angles-in-python/
+def euler_from_quaternion(x, y, z, w):
+    """
+    Convert a quaternion into euler angles (roll, pitch, yaw)
+    roll is rotation around x in radians (counterclockwise)
+    pitch is rotation around y in radians (counterclockwise)
+    yaw is rotation around z in radians (counterclockwise)
+    """
+    t0 = +2.0 * (w * x + y * z)
+    t1 = +1.0 - 2.0 * (x * x + y * y)
+    roll_x = math.atan2(t0, t1)
+
+    t2 = +2.0 * (w * y - z * x)
+    t2 = +1.0 if t2 > +1.0 else t2
+    t2 = -1.0 if t2 < -1.0 else t2
+    pitch_y = math.asin(t2)
+
+    t3 = +2.0 * (w * z + x * y)
+    t4 = +1.0 - 2.0 * (y * y + z * z)
+    yaw_z = math.atan2(t3, t4)
+
+    return roll_x, pitch_y, yaw_z # in radians
+
 def get_vehicle_state(data):
     
     global pos,velocity,old_pos
@@ -161,7 +185,9 @@ def get_vehicle_state(data):
             data.pose[2].orientation.y,
             data.pose[2].orientation.z,
             data.pose[2].orientation.w)
-    euler = tf.transformations.euler_from_quaternion(quaternion)
+    #euler = tf.transformations.euler_from_quaternion(quaternion)
+    q = quaternion
+    euler =  euler_from_quaternion(q[0],q[1],q[2],q[3])
     yaw = euler[2]
     yaw_car = yaw
     velocity = get_current_velocity(old_pos[0],old_pos[1],pos[0],pos[1])